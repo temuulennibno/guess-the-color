ead9a45a8068ced00c9869109c1652613e6ee13f
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-01-08 14:05:00 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, Touchable, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import { Container } from "./components/container";
+import { Dimensions } from "react-native";
+import { useEffect, useRef, useState } from "react";
+
+type Tile = {
+  color: string;
+  isOdd: boolean;
+};
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [seconds, setSeconds] = useState(60);
+
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const [tiles, setTiles] = useState<Tile[]>([
+    { color: "red", isOdd: false },
+    { color: "red", isOdd: false },
+    { color: "blue", isOdd: true },
+  ]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      clearInterval(intervalRef.current!);
+    }
+    intervalRef.current = setInterval(() => {
+      setSeconds((seconds) => (seconds > 0 ? seconds - 1 : 0));
+    }, 1000);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [seconds]);
+
+  const handleCorrect = () => {
+    setScore(score + 1);
+    setSeconds(seconds + 10);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Text style={styles.title}>
+        Өөр <Text style={styles.oddTitle}>өнгийг</Text> ол!
+      </Text>
+      <View style={{ padding: 16, display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+        <Text>{seconds} секунд</Text>
+        <Text style={styles.score}>Оноо {score}</Text>
+      </View>
+      <View style={styles.board}>
+        {tiles.map((tile, index) => (
+          <View style={styles.tileGrid} key={`tile-${index}`}>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                if (tile.isOdd) {
+                  handleCorrect();
+                }
+              }}
+            >
+              <View style={{ ...styles.tile, backgroundColor: tile.color }}></View>
+            </TouchableWithoutFeedback>
+          </View>
+        ))}
+      </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  oddTitle: {
+    color: "rgba(0,0,0,0.5)",
+  },
+  score: {
+    color: "#363636",
+  },
+  board: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    gap: 8,
+  },
+  tileGrid: {
+    width: (Dimensions.get("window").width - 32) / 3,
+  },
+  tile: {
+    width: "100%",
+    height: 0,
+    paddingBottom: "100%",
+    backgroundColor: "red",
+    borderRadius: 10,
   },
 });
---
+++ components/container.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export const Container = ({ children }: React.PropsWithChildren) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Constants.statusBarHeight,
+  },
+});
